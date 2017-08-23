757c04e078192feec159a87dd9493ace17321d30
LCJ-MinYa <1049468118@qq.com>
2017-08-23 10:12:13 +0800

    init 百思不得姐

---
+++ 百思不得姐/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  百思不得姐
+//
+//  Created by minya on 2017/8/23.
+//  Copyright © 2017年 LiChaoJun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
