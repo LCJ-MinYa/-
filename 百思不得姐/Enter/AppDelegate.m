c8e51f13965365d126427c3108d070e6353a1d7e
LCJ-MinYa <1049468118@qq.com>
2017-08-23 10:48:43 +0800

    初始化项目资源

---
+++ 百思不得姐/Enter/AppDelegate.m
@@ -0,0 +1,44 @@
+//
+//  AppDelegate.m
+//  百思不得姐
+//
+//  Created by minya on 2017/8/23.
+//  Copyright © 2017年 LiChaoJun. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    // Override point for customization after application launch.
+    return YES;
+}
+
+
+- (void)applicationWillResignActive:(UIApplication *)application {
+}
+
+
+- (void)applicationDidEnterBackground:(UIApplication *)application {
+}
+
+
+- (void)applicationWillEnterForeground:(UIApplication *)application {
+}
+
+
+- (void)applicationDidBecomeActive:(UIApplication *)application {
+}
+
+
+- (void)applicationWillTerminate:(UIApplication *)application {
+}
+
+
+@end
