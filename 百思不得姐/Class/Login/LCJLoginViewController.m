17963edb85187de8c14031ff4699f60e47175712
LCJ-MinYa <1049468118@qq.com>
2017-08-25 18:09:34 +0800

    纯代码ui遇到问题

--- 百思不得姐/Class/Login/LCJLoginViewController.m
+++ 百思不得姐/Class/Login/LCJLoginViewController.m
@@ -19,9 +19,18 @@
     
     //设置导航器相关
     [self setNav];
+    
+    //顶部关闭和切换注册登录
+    
+    //第三方登录view
+    [self setThirdPartyLogin];
 }
 
 #pragma mark 设置状态栏文本颜色
+/*
+ *  -(BOOL)prefersStatusBarHidden
+ *  控制状态栏是否显示
+ */
 -(UIStatusBarStyle)preferredStatusBarStyle
 {
     return UIStatusBarStyleLightContent;
@@ -34,6 +43,41 @@
     UIImage * backGroundImage=[UIImage imageNamed:@"login_register_background"];
     self.view.contentMode = UIViewContentModeScaleAspectFill;
     self.view.layer.contents = (__bridge id _Nullable)(backGroundImage.CGImage);
+}
+
+#pragma mark 设置第三方登录[UI]
+-(void)setThirdPartyLogin
+{
+    UIView * thirdPartyLoginView = [[UIView alloc] initWithFrame:CGRectMake(0, SCREEN_HEIGHT - 180, SCREEN_WIDTH, 150)];
+    thirdPartyLoginView.backgroundColor = [UIColor clearColor];
+    
+    UIView * thirdPartyLoginTitle = [self setThirdPartyLoginTitle];
+    [thirdPartyLoginView addSubview:thirdPartyLoginTitle];
+    [self.view addSubview:thirdPartyLoginView];
+}
+
+#pragma mark 设置第三方登录标题[UI]
+-(UIView *)setThirdPartyLoginTitle
+{
+    UIView * view = [[UIView alloc] initWithFrame:CGRectMake(0, 0, SCREEN_WIDTH, 30)];
+    
+    //创建标题
+    UILabel * title = [[UILabel alloc] init];
+    title.text = @"快速登录";
+    title.font = [UIFont systemFontOfSize:14];
+    [title setTextColor:[UIColor whiteColor]];
+    [title sizeToFit];
+    title.center = CGPointMake(SCREEN_WIDTH / 2, 15);
+    [view addSubview:title];
+    LCJLog(@"%@", NSStringFromCGRect(title.frame));
+    
+    //左边线
+    UIImageView * leftImg = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"login_register_left_line"]];
+    [leftImg sizeToFit];
+    LCJLog(@"%@", NSStringFromCGRect(leftImg.frame));
+    [view addSubview:leftImg];
+    
+    return view;
 }
 
 @end
