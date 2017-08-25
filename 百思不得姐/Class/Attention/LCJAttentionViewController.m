b00e1eb02d537e9cc94d0ce2065a949868c6ec8d
LCJ-MinYa <1049468118@qq.com>
2017-08-25 15:07:41 +0800

    关注页面，所有统一背景还是需要在自己视图单独设置

--- 百思不得姐/Class/Attention/LCJAttentionViewController.m
+++ 百思不得姐/Class/Attention/LCJAttentionViewController.m
@@ -11,6 +11,8 @@
 
 @interface LCJAttentionViewController ()
 
+@property (nonatomic, weak) UILabel * label;
+
 @end
 
 @implementation LCJAttentionViewController
@@ -19,12 +21,56 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    //设置导航器相关
+    [self setNav];
+
+    //提示文字
+    [self setHintText];
+    
+    //提示图片
+    [self setHintImg];
+}
+
+#pragma mark 设置导航器相关
+-(void)setNav
+{
+    //设置背景颜色
+    self.view.backgroundColor = LCJCommonBgColor;
+    
     //标题
     self.navigationItem.title = @"我的关注";
-    //左边按钮
+    
     //左边按钮
     UIBarButtonItem * leftButton = [LCJNavButtonItem itemWithImage:@"friendsRecommentIcon" highImage:@"friendsRecommentIcon-click" target:self action:@selector(leftButtonClick)];
     self.navigationItem.leftBarButtonItems = @[leftButton];
+}
+
+#pragma mark 设置提示文字
+-(void)setHintText
+{
+    //提示文字
+    UILabel * label = [[UILabel alloc] init];
+    label.numberOfLines = 0;
+    label.text = @"快快登录吧，关注百思最in牛人\n好友动态让你过把瘾儿\n欧耶～～～！";
+    label.textColor = LCJColor(50, 50, 50);
+    label.font = [UIFont systemFontOfSize:14];
+    label.textAlignment = NSTextAlignmentCenter;
+    CGSize size = [label sizeThatFits:CGSizeMake(SCREEN_WIDTH * 0.7, MAXFLOAT)];
+    label.frame = CGRectMake(0, 0, size.width, size.height);
+    label.center = CGPointMake(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2);
+    self.label = label;
+    [self.view addSubview:label];
+}
+
+#pragma mark 设置提示图片
+-(void)setHintImg
+{
+    UIImageView * image = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"header_cry_icon"]];
+    [image sizeToFit];
+    image.lcj_centerX = SCREEN_WIDTH / 2;
+    image.lcj_centerY = self.label.frame.origin.y - 24 - 20;
+    LCJLog(@"%@", NSStringFromCGRect(image.frame));
+    [self.view addSubview:image];
 }
 
 #pragma mark 左侧按钮点击事件
--- 百思不得姐/Class/Main/LCJNavigationController.m
+++ 百思不得姐/Class/Main/LCJNavigationController.m
@@ -8,7 +8,7 @@
 
 #import "LCJNavigationController.h"
 
-@interface LCJNavigationController ()
+@interface LCJNavigationController () <UIGestureRecognizerDelegate>
 
 @end
 
@@ -17,6 +17,15 @@
 #pragma mark 初始化
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    /*
+     * 重定义左侧按钮会影响右滑返回
+     * 需要自己再添加上
+     */
+    self.interactivePopGestureRecognizer.delegate = self;
+    
+    //设置导航控制器的背景图片
+    [self.navigationBar setBackgroundImage:[UIImage imageNamed:@"navigationbarBackgroundWhite"] forBarMetrics:UIBarMetricsDefault];
 }
 
 #pragma mark 重写push方法，里面设置返回键
@@ -40,8 +49,14 @@
         viewController.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:backButton];
     }
     
-    //统一设置背景色
-    viewController.view.backgroundColor = LCJRandomColor;
+    /*
+     *  统一不能设置背景色
+     *  凡是调用子控制器的view都会触发viewDidLoad方法，导致渲染，不能懒加载每个视图（即点击那个视图才显示哪个视图）
+     *  viewController.view.backgroundColor = LCJCommonBgColor;
+     *  解决办法:
+     *  创建一个继承于UIViewController的类，然后其他视图所有继承这个类，在此类中定义统一背景
+     *  特殊页面也可以自行重写背景颜色
+     */
     
     //下一个页面设置完毕后再push进去
     [super pushViewController:viewController animated:animated];
@@ -58,5 +73,21 @@
 {
     [self popViewControllerAnimated:YES];
 }
+
+#pragma mark 添加手势返回代理
+-(BOOL)gestureRecognizerShouldBegin:(UIGestureRecognizer *)gestureRecognizer
+{
+    /*
+     *  返回值决定手势是否有效
+     *  当导航控制器的子控制器只有一个视图时，即返回到当前控制器的主页，需要返回NO，即不允许手势实效
+     *  当导航控制器的子控制器超过一个视图时，即非主页都允许返回，返回YES
+     *  if(self.childViewControllers.count == 1){
+     *      return NO;
+     *  }
+     *  return YES;
+     */
+    return self.childViewControllers.count > 1;
+}
+
 
 @end
